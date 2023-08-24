80444a887f75b3dc895c00713503cd1c8b91763b
Leonardo Luís Tozoni <47570204+Leonardo-Tozoni@users.noreply.github.com>
2023-08-24 11:19:13 -0300

    Scroll suave e link externo

--- script.js
+++ script.js
@@ -41,52 +41,28 @@ function initAccordion() {
 }
 initAccordion();
 
-function initScrollSuave () {
-  const linksInternos = document.querySelectorAll('js-menu a[href^="#"]');
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-  function scrollToSection (event) {
+  function scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-
     section.scrollIntoView({
       behavior: 'smooth',
       block: 'start',
     });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
   }
-  // Forma alternativa
-  //   const topo = section.offsetTop;
-  //   window.scrollTo({
-  //     top: 1000,
-  //     behavior: 'smooth'
-  //   });
-  // }
 
   linksInternos.forEach((link) => {
     link.addEventListener('click', scrollToSection);
   });
 }
 initScrollSuave();
-
-function initAnimacaoScroll(){
-  const sections = document.querySelectorAll('.js-scroll');
-
-  if(sections.length){
-    const metadeWindow = window.innerHeight * 0.6;
-
-    function animaScroll (){
-      sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - metadeWindow) < 0;
-        if(isSectionVisible) {
-          section.classList.add('ativo');
-        }
-        else
-          section.classList.remove('ativo');
-      })
-    }
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
-  }
-}
-initAnimacaoScroll();
